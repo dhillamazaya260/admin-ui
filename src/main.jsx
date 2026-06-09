c8eb6938c762e3550e9f929a723943a2f0c10cdc
dhillamazaya260 <dhillamazaya9@gmail.com>
2026-06-09 16:39:06 +0700

    Latihan - Get Form Value

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
-import { ThemeContextProvider } from "./context/themeContext.jsx";
+import Form from "./latihan/Form.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeContextProvider>
-      <App />
-    </ThemeContextProvider>
-  </React.StrictMode>,
+    <Form />
+  </React.StrictMode>
 );
