6c647880b8747fed9d6676581e86c0ac08677796
dhillamazaya260 <dhillamazaya9@gmail.com>
2026-06-09 16:56:17 +0700

    Latihan - Get Data

--- src/latihan/form.jsx
+++ src/latihan/form.jsx
@@ -1,23 +1,43 @@
-import React, { useState } from "react";
-import axios from "axios";   // ← tambahkan ini
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Form = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [goals, setGoals] = useState({});
 
   const handleSubmit = async (e) => {
-  e.preventDefault();
-  try {
-    const response = await axios.post(
-      "https://jwt-auth-eight-neon.vercel.app/login",
-      { email, password },
-    );
-    console.log(response.data);
-    localStorage.setItem("token", response.data.refreshToken);
-  } catch (error) {
-    console.error(error);
-  }
-};
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "https://jwt-auth-eight-neon.vercel.app/login",
+        { email, password },
+      );
+      console.log(response.data);
+      localStorage.setItem("token", response.data.refreshToken);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchGoals = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const response = await axios.get("https://jwt-auth-eight-neon.vercel.app/goals", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      console.log(response.data);
+      setGoals(response.data.data[0]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchGoals();
+  }, []);
 
   return (
     <div className="p-4">
@@ -44,6 +64,10 @@ const Form = () => {
         <br />
         <input type="submit" value="send" className="bg-gray-200 p-2" />
       </form>
+      <hr className="py-4" />
+      Present amount : {goals.present_amount}
+      <br />
+      Target Amount : {goals.target_amount}
     </div>
   );
 };
